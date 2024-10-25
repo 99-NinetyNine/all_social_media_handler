1702bb880f1350104c9e9a93c01a1b2c8fca3fd5
sailesh <saileshshiwakoti2@gmail.com>
2024-10-25 17:25:22 +0545

    first lets finish fb

---
+++ src/fb/tests/fb_post.py
@@ -0,0 +1,24 @@
+"""\
+link is found here
+#https://developers.facebook.com/docs/pages-api/posts/
+
+this shows how to post text.
+
++ 
+
+how to send a photo
+
+,. To send multiple photoos:
+https://stackoverflow.com/questions/23015640/how-do-i-can-post-a-multiple-photos-via-facebook-api
+
+and to send video separate logic is there
+https://developers.facebook.com/docs/video-api/guides/publishing
+
+moreover, 
+to send the story:
+https://developers.facebook.com/docs/page-stories-api
+
+## for reference code, there is sdk available on pypi
+
+
+"""
